cc333dbc259528b678618aab225f112f8c0ab20b
ndlrfz <nadlir2596@gmail.com>
2025-11-28 00:10:23 +0700

    **idk**

--- lisp/org-config.el
+++ lisp/org-config.el
@@ -1,47 +1,88 @@
-;; org-mode
-;; Make sure code blocks use fixed-pitch for correct visual indentation
-
 (use-package org
-  :hook ((org-mode . visual-line-mode)
-         (org-mode . variable-pitch-mode)
-         (org-mode . org-indent-mode))
+  :ensure nil  ;; org bawaan Emacs, jangan install dari ELPA
   :config
-  (setq org-hide-leading-stars t
-        org-ellipsis " ▾"
-        org-pretty-entities t
-        org-startup-folded t
-        org-src-tab-acts-natively t
-        org-src-preserve-indentation nil
-        org-edit-src-content-indentation 2
-        org-hide-emphasis-markers t))
+  ;; Directory untuk org file
+  ;;(setq org-directory "~/org")
+  
 
+  ;; Default notes file
+  (setq org-default-notes-file (expand-file-name "notes.org" org-directory))
 
-(setq org-adapt-indentation t)
+  ;; Indentasi rapi
+  (setq org-startup-indented t)
+
+  ;; Auto wrap line
+  (setq org-startup-truncated nil)
+
+  ;; elipsis
+  (setq org-ellipsis " ▾")
+
+  ;; Log timestamps
+  (setq org-log-done 'time
+        org-log-into-drawer t)
+
+  (add-hook 'org-mode-hook
+            (lambda ()
+              (setq line-spacing 0.3)))   ;; 0.2 = sedikit renggang, sesuaikan
+
+  (add-hook 'org-mode-hook
+            (lambda ()
+              (display-line-numbers-mode -1)))
+
+  (add-hook 'org-mode-hook
+            (lambda ()
+              (local-set-key (kbd "TAB") #'org-cycle)))
+
+  ;; Body text
+  (set-face-attribute 'org-default nil :family "Fira Sans" :height 120)
+
+  ;; Headings (ukuran berbeda-beda)
+  (set-face-attribute 'org-level-1 nil :family "Fira Sans" :height 160 :weight 'medium)
+  (set-face-attribute 'org-level-2 nil :family "Fira Sans" :height 150 :weight 'regular)
+  (set-face-attribute 'org-level-3 nil :family "Fira Sans" :height 140)
+  (set-face-attribute 'org-level-4 nil :family "Fira Sans" :height 130)
+  (set-face-attribute 'org-level-5 nil :family "Fira Sans" :height 125)
+  (set-face-attribute 'org-level-6 nil :family "Fira Sans" :height 120)
+
+  ;; Jangan ubah font untuk code, src block, table, dll
+  (set-face-attribute 'org-code nil :family "Zenbones Mono")
+  (set-face-attribute 'org-block nil :family "Zenbones Mono")
+  (set-face-attribute 'org-table nil :family "Zenbones Mono")
+
+  ;; Images auto display
+  (setq org-startup-with-inline-images t))
+
+;; Kalau pakai treesitter Emacs 29+
 (with-eval-after-load 'org
-  (set-face-attribute 'org-block nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-meta-line nil :inherit 'fixed-pitch))
+  (setq org-element-use-cache nil))
+
+;; 10) Small quality-of-life tweaks
+(with-eval-after-load 'org
+  (setq org-hide-emphasis-markers t    ;; hide /bold/ markers
+        org-use-sub-superscripts '{}
+        org-startup-with-inline-images t
+        org-image-actual-width '(300))) ;; scale inline images
 
 (use-package org-modern
+  :straight t
   :hook (org-mode . org-modern-mode)
   :config
-  (setq org-modern-star '("●" "○" "◆" "◇" "▹")
-        org-modern-block-fringe 2
-        org-modern-table t
-        org-modern-variable-pitch t
-        org-modern-todo t))
+  (setq org-modern-hide-stars t
+        org-modern-list-spacing 4
+        org-modern-star '("● " "○ " "◆ " "◇ " "▶ ")))
 
+;; --- nicer faces for headings (scale) ---
+;; (use-package org-indent
+;;   :ensure nil
+;;   :hook (org-mode . org-indent-mode))
 
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "IN-PROGRESS(i)" "|" "DONE(d)" "CANCELLED(c)")))
+(setq org-lowest-priority ?F)  ;; Gives us priorities A through F
+(setq org-default-priority ?E) ;; If an item has no priority, it is considered [#E].
 
-(setq org-todo-keyword-faces
-      '(("TODO" . "red")
-        ("IN-PROGRESS" . "yellow")
-        ("DONE" . "green")
-        ("CANCELLED" . "gray")))
-
-(use-package org-appear
-  :hook (org-mode . org-appear-mode)
-  :config
-  (setq org-appear-autolinks t
-        org-appear-autosubmarkers t))
+(setq org-capture-templates
+      '(("t" "Todo" entry
+         (file+headline "~/todo.org" "Tasks")
+         "* TODO %?\n  %U\n")
+        ("n" "Note" entry
+         (file+headline "~/notes.org" "Notes")
+         "* %?\n  %U\n")))
